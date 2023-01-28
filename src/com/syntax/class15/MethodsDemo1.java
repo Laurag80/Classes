4bc67aa3121ef052931c1abe7c084b9e4a722022
Laurag <lauragasparyan87@gmail.com>
2023-01-28 12:26:17 -0800

    class15

---
+++ src/com/syntax/class15/MethodsDemo1.java
@@ -0,0 +1,15 @@
+package com.syntax.class15;
+
+import java.util.Scanner;
+
+public class MethodsDemo1 {
+    public static void main(String[] args) {
+
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please Enter a number");
+        int num=scanner.nextInt();
+
+        System.out.println("you entered "+num);
+    }
+}
---
+++ src/com/syntax/class15/Task.java
@@ -0,0 +1,14 @@
+package com.syntax.class15;
+
+public class Task {
+    /*
+    Create a method createEmail(). Based on values of users name,
+    lastName and email type, your method should return complete email Address.
+    Example:  createEmail(John, Snow, gmail) → johnsnow@gmail.com or
+     */
+
+    String createEmail(String firstName,String lastName,String emailType){
+        return firstName+lastName+"@"+emailType+".com";
+    }
+    }
+
